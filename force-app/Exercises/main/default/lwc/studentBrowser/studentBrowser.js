4071fdf1f2f3dff4f53b6f1af311556fdc7c4d40
ZiyadAhmed910 <ziyadahmed910@gmail.com>
2022-10-18 13:32:12 +0530

    Control The gallery

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,7 +1,15 @@
 import { LightningElement ,wire} from 'lwc';
 import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
+import DetailUrl from '@salesforce/schema/EntityDefinition.DetailUrl';
 
 export default class StudentBrowser extends LightningElement {
     // studenList = [];
-    @wire (getStudents,{instructorId:"", courseDeliveryId:""}) students;
+    selectedDeliveryId = '';
+    selectedInstructorId = '';
+    @wire (getStudents,{instructorId:'$selectedInstructorId', courseDeliveryId:'$selectedDeliveryId'}) students;
+    
+    handleFilterChnage(event){
+        this.selectedDeliveryId = event.detail.deliveryId;
+        this.selectedInstructorId = event.detail.instructorId;
+    }
 }
