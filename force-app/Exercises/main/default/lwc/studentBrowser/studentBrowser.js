f494e577288f9604e52247420947e83799cad877
ZiyadAhmed910 <ziyadahmed910@gmail.com>
2022-10-18 16:41:36 +0530

    Adding a map done excersise 3-1

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,14 +1,14 @@
-import { LightningElement ,wire} from 'lwc';
+import { LightningElement, wire} from 'lwc';
 import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
-import DetailUrl from '@salesforce/schema/EntityDefinition.DetailUrl';
 
 export default class StudentBrowser extends LightningElement {
-    // studenList = [];
-    @wire (getStudents,{instructorId:'$selectedInstructorId', courseDeliveryId:'$selectedDeliveryId'}) students;
-    selectedDeliveryId = '';
-    selectedInstructorId = '';
+    // studentList=[];
+    @wire (getStudents, {instructorId:'$selectedInstructorId',courseDeliveryId:'$selectedDeliveryId'}) students;
+    
+    selectedDeliveryId='';
+    selectedInstructorId='';
     handleFilterChange(event){
-        this.selectedDeliveryId = event.detail.deliveryId;
-        this.selectedInstructorId = event.detail.instructorId;
+        this.selectedInstructorId=event.detail.instructorId;
+        this.selectedDeliveryId=event.detail.deliveryId;
     }
 }
