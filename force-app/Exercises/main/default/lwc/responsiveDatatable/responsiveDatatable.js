0f1b69ae5262b2647e48adf915752077b776d12a
ZiyadAhmed910 <ziyadahmed910@gmail.com>
2022-10-20 18:20:41 +0530

    updated responsiveDatable.js

--- force-app/Exercises/main/default/lwc/responsiveDatatable/responsiveDatatable.js
+++ force-app/Exercises/main/default/lwc/responsiveDatatable/responsiveDatatable.js
@@ -1,42 +1,21 @@
 import { LightningElement, api } from 'lwc';
 
 export default class ResponsiveDatatable extends LightningElement {
-
-	@api columnConfig;
+	
+	@api columnConfig; //array of objecs like [ { fieldName: 'Email', label:'E-mail' }, { fieldName:'Phone', label: 'Phone Number', type: 'phone'}]
 	@api pkField;
 	rows;
+	_selectedRow;
 
-	@api get rowData(){
-		return this.rows;		
-	}	
-
-	set rowData(value) {
-		if (typeof value !== "undefined") {
-		this.rows = this.reformatRows(value);
-		}
+	@api
+	get rowData() {
+		return this.rows;
 	}
-
-	cols = [
-		{
-		fieldName:"Name",
-		label: "Name"
-		},
-		{
-		fieldName:"Title",
-		label: "Title",
-		hiddenOnMobile: true
-		},
-		{
-		fieldName:"Phone",
-		label: "Phone",
-		type: "phone"
-		},
-		{
-		fieldName:"Email",
-		label: "E-Mail",
-		type: "email"
-		}
-		];
+	set rowData(value) {
+		if (typeof value !== 'undefined') {
+			this.rows = this.reformatRows(value);
+		} 
+	}
 	
 	reformatRows = function(rowData) {
 		let colItems = this.columnConfig;
@@ -54,7 +33,6 @@ export default class ResponsiveDatatable extends LightningElement {
 					label: colItems[j].label,
 					type: colItems[j].type,
 					class: colClass,
-					columnId: 'col' + j + '-' + rowData[i][this.pkField],
 					isPhone: (colItems[j].type==='phone'),
 					isEmail: (colItems[j].type==='email'),
 					isOther: (colItems[j].type!=='phone' && colItems[j].type!=='email')
@@ -68,4 +46,35 @@ export default class ResponsiveDatatable extends LightningElement {
 		return reformattedRows;
 	}
 
+	onRowClick(event) {
+		let target = event.currentTarget;
+		const evt = new CustomEvent( 'rowclick' , {
+			detail: {
+				pk: target.getAttribute('data-pk')
+			}
+		});
+		this.dispatchEvent(evt);
+		this.highlightSelectedRow(target);
+	}
+	
+	onRowDblClick(event) {
+		let target = event.currentTarget;
+		const evt = new CustomEvent( 'rowdblclick' , {
+			detail: {
+				pk: target.getAttribute('data-pk')
+			}
+		});
+		this.dispatchEvent(evt);
+	}
+
+	highlightSelectedRow(target) {
+		if (this._selectedRow) {
+			this._selectedRow.classList.remove("slds-is-selected");
+		}
+		target.classList.add("slds-is-selected");
+		this._selectedRow = target;
+	}
+
+	
+	
 }
