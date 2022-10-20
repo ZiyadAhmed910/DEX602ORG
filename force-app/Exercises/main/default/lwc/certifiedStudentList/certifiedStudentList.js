e8d678d73a93775b0b3879743d713e0edcf80095
ZiyadAhmed910 <ziyadahmed910@gmail.com>
2022-10-20 14:00:52 +0530

    added validation

--- force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
+++ force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
@@ -1,78 +1,81 @@
-import { LightningElement,wire,api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import getCertifiedStudents from '@salesforce/apex/CertifiedStudentList.getCertifiedStudents';
-import deleteStudentCertification from'@salesforce/apex/CertifiedStudentList.deleteStudentCertification';
+import deleteStudentCertification from '@salesforce/apex/CertifiedStudentList.deleteStudentCertification';
 import { refreshApex } from '@salesforce/apex';
-
 export default class CertifiedStudentList extends LightningElement {
     @api certificationId = 0;
     @api certificationName = '';
-    certifiedStudents;
     btnGroupDisabled = true;
-    error;
+    certifiedStudents;
     _wiredStudentResult;
-    @wire(getCertifiedStudents,{certificationId: '$certificationId'}) wired_getCertifiedStudents(result) {
-        this._wiredStudentResult = result;
+    error;
+    @wire(getCertifiedStudents,
+        { certificationId: '$certificationId' })
+    wired_getCertifiedStudents(result) {
         this.certifiedStudents = [];
+        this._wiredStudentResult = result;
         if (result.data) {
             this.certifiedStudents = result.data.map(certHeld =>
-                ({certificationHeldId: certHeld.Id,
+            ({
+                certificationHeldId: certHeld.Id,
                 contactId:
-                certHeld.Certified_Professional__r.Id,
+                    certHeld.Certified_Professional__r.Id,
                 name: certHeld.Certified_Professional__r.Name,
                 date: certHeld.Date_Achieved__c,
                 email:
-                certHeld.Certified_Professional__r.Email,
+                    certHeld.Certified_Professional__r.Email,
                 phone:
-                certHeld.Certified_Professional__r.Phone})
+                    certHeld.Certified_Professional__r.Phone
+            })
             );
         } else if (result.error) {
             this.error = result.error;
         }
-}
-columnConfig = [
-    {
-    label: 'Name',
-    fieldName: 'name',
-    type: 'text'
-    },{
-    label: 'Date',
-    fieldName: 'date',
-    type: 'text'
-    },{
-    label: 'Email',
-    fieldName: 'email',
-    type: 'email'
-    },{
-    label: 'Phone',
-    fieldName: 'phone',
-    type: 'phone'
     }
+    columnConfig = [
+        {
+            label: 'Name',
+            fieldName: 'name',
+            type: 'text'
+        }, {
+            label: 'Date',
+            fieldName: 'date',
+            type: 'text'
+        }, {
+            label: 'Email',
+            fieldName: 'email',
+            type: 'email'
+        }, {
+            label: 'Phone',
+            fieldName: 'phone',
+            type: 'phone'
+        }
     ];
     onRowSelection(event) {
         const numSelected = event.detail.selectedRows.length;
         this.btnGroupDisabled = (numSelected === 0);
-        
     }
-
     getSelectedIDs() {
-        const datatable = this.template.querySelector('lightning-datatable');
-        const ids = datatable.getSelectedRows().map( (r) => (
-            r.certificationHeldId
-        ));
-        return ids;
+        const datatable =
+this.template.querySelector('lightning-datatable');
+const ids = datatable.getSelectedRows().map( (r) => (
+    r.certificationHeldId
+    ));
+    return ids;
     }
     onCertActions (event) {
-        const btnClicked = event.target.getAttribute('data-btn-id');
-        switch (btnClicked) {
-            case 'btnEmail':
-            break;
-            case 'btnSendCert':
-            break;
-            case'btnDelete': this.onDelete();
-            break;
-            default:
-            break;
-        }
+        const btnClicked =
+event.target.getAttribute('data-btn-id');
+switch (btnClicked) {
+    case 'btnEmail':
+    break;
+    case 'btnSendCert':
+        break;
+case 'btnDelete':
+this.onDelete();
+break;
+default:
+break;}
     }
     onDelete() {
         const certificationIds = this.getSelectedIDs();
@@ -82,5 +85,6 @@ columnConfig = [
             .catch(error => {
             this.error = error;
             });
-    }
+        }
+        
 }
